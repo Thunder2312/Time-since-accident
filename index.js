b6bf27dbc4dbd2a564a56180a3a633e74797ee8d
SD <77830487+Thunder2312@users.noreply.github.com>
2024-07-03 16:06:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const accidentDate = new Date('2024-07-01T09:39:00');
+
+function updateCountdown() {
+    const currentDate = new Date();
+    const timeDifference = currentDate.getTime() - accidentDate.getTime();
+    const daysSinceAccident = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hoursSinceAccident = Math.floor(timeDifference/ (1000 * 60 * 60) % 24);
+    const minutesSinceAccident = Math.floor(timeDifference / (1000 * 60) % 60);
+    const secondsSinceAccident = Math.floor(timeDifference / 1000 % 60);
+
+    document.getElementById('countdown').innerHTML = `${daysSinceAccident} days ${hoursSinceAccident} hours ${minutesSinceAccident} minutes ${secondsSinceAccident} seconds`;
+    if(daysSinceAccident < 10){
+        document.getElementById("countdown").style.color="red";
+    }
+    else if(daysSinceAccident > 10 && daysSinceAccident < 100){
+        document.getElementById("countdown").style.color="yellow";
+    }
+    else{
+        document.getElementById("countdown").style.color="green";
+    }
+}
+
+// Update every second
+setInterval(updateCountdown, 1000);
+
+// Initial call to display the correct countdown immediately
+updateCountdown();
